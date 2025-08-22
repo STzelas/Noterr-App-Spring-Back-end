b67398dcb14534a37dece021eacc66300968daa7
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-22 16:16:56 +0300

    feat(UserInsertDTO): @Pattern bean validation added

--- src/main/java/com/stzelas/gr/notes_app_api/dto/UserInsertDTO.java
+++ src/main/java/com/stzelas/gr/notes_app_api/dto/UserInsertDTO.java
@@ -1,21 +1,26 @@
 package com.stzelas.gr.notes_app_api.dto;
 
-import com.stzelas.gr.notes_app_api.core.enums.Role;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserInsertDTO(
 
         @NotEmpty(message = "Το username δεν μπορεί να είναι κενό")
         @Size(min = 4, max = 15, message = "Το username πρέπει να είναι μεταξύ 4 και 15 χαρακτήρων")
+        @Pattern(regexp = "^\\S+$", message = "Δεν επιτρέπονται white spaces.")
         String username,
         @NotEmpty(message = "Το password δεν μπορεί να είναι κενό")
         @Size(min = 4, max = 15, message = "Το password πρέπει να είναι τουλάχιστον 6 χαρακτήρες")
+        @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\\S{6,}$",
+                message = "Ο κωδικός πρέπει να περιέχει 1 τουλάχσιτον μικρό και κεφαλαίο γράμμα και 1 αριθμό χωρίς κενά.")
         String password,
         @NotEmpty(message = "Το όνομα δεν μπορεί να είναι κενό")
         @Size(min = 4, max = 15, message = "Το password όνομα να είναι τουλάχιστον 3 χαρακτήρες")
+        @Pattern(regexp = "^\\S+$", message = "Δεν επιτρέπονται white spaces.")
         String firstname,
         @NotEmpty(message = "Το επίθετο δεν μπορεί να είναι κενό")
         @Size(min = 4, max = 15, message = "Το επίθετο πρέπει να είναι τουλάχιστον 3 χαρακτήρες")
+        @Pattern(regexp = "^\\S+$", message = "Δεν επιτρέπονται white spaces.")
         String lastname
 ) {}
